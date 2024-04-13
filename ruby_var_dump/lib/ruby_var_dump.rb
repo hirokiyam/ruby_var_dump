d420b0e145c74537bf250d62e849a6c4e2099212
hirokiyam <distant_record@hotmail.co.jp>
2024-04-14 04:33:54 +0900

    execute bundle gem ruby_var_dump

---
+++ ruby_var_dump/lib/ruby_var_dump.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ruby_var_dump/version"
+
+module RubyVarDump
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ ruby_var_dump/lib/ruby_var_dump/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RubyVarDump
+  VERSION = "0.1.0"
+end
