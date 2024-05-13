a0b4e557b639fe7aee22ed1f5c8a48f4fa8331eb
hirokiyam <distant_record@hotmail.co.jp>
2024-05-14 01:25:12 +0900

    Show ActiveRecord associations

--- ruby_var_dump/lib/ruby_var_dump.rb
+++ ruby_var_dump/lib/ruby_var_dump.rb
@@ -5,7 +5,7 @@ require_relative "ruby_var_dump/version"
 module RubyVarDump
   class Error < StandardError; end
 
-  def dump(obj, klass=obj.class, level = 0, is_value = false, dumped_objects = [])
+  def dump(obj, klass=obj.class, level = 0, is_value = false, dumped_objects = [], is_first=true)
     indent = ' ' * level * 2
 
     if dumped_objects.include?(obj.object_id)
@@ -23,7 +23,7 @@ module RubyVarDump
       else
         print "\n"
         obj.each_with_index do |item, index|
-          dump(item, item.class, level + 1, false)
+          dump(item, item.class, level + 1, false, dumped_objects, false)
           print "," unless index == obj.size - 1
           print "\n"
         end
@@ -41,7 +41,7 @@ module RubyVarDump
           # キーにインデントを適用し、値にはインデントを適用しない
           print "#{indent}  #{key.inspect.chomp} => "
           if value.is_a?(Hash) || value.is_a?(Array)
-            dump(value, level + 1, true)  # ハッシュのバリューの場合には is_value を true に設定
+            dump(value, value.class, level + 1, true, dumped_objects, false)  # ハッシュのバリューの場合には is_value を true に設定
           else
             print value.inspect  # プリミティブな値の場合は現在のレベルで出力
           end
@@ -53,6 +53,7 @@ module RubyVarDump
       end
     elsif defined?(ActiveRecord::Base) && obj.is_a?(ActiveRecord::Base)
       # ActiveRecordオブジェクトの場合
+      print "\n" unless is_first  # 最初のレコード以外の場合に改行を入れる
       print "#{indent}#{obj.class}:#{obj.object_id}\n"
       print "#{indent}{\n"
       obj.attributes.each do |attr_name, attr_value|
@@ -61,14 +62,14 @@ module RubyVarDump
       # リレーションも再帰的にダンプ
       obj.class.reflect_on_all_associations.each do |association|
         associated_value = obj.send(association.name)
-        print "#{indent}  #{association.name}:"
+        print "#{indent}  << #{association.name} >> (association):"
         if associated_value.respond_to?(:each) # アソシエーションがコレクションの場合
-          break if associated_value.nil? || associated_value.empty?
+          next if associated_value.nil? || associated_value.empty?
           print("\n#{indent}  [\n")
           associated_value.each do |item|
-            break if dumped_objects.include?(item.object_id)
+            next if dumped_objects.include?(item.object_id)
     
-            # CollectionProxy の内容をダンプ
+            # CollectionProxy の内容をダンプ 
             print("#{indent}    #{association.name}: #{item.class}:#{item.object_id}\n")
             print("#{indent}    {\n")
             item.attributes.each do |attr_name, attr_value|
@@ -80,19 +81,19 @@ module RubyVarDump
           print("#{indent}  ]\n")
         end
       end
-      print "#{indent}}"
+      print "#{indent}}\n"
       print "\n" if level == 0
     elsif defined?(ActiveRecord::Relation) && obj.is_a?(ActiveRecord::Relation)
       # ActiveRecord::Relation の場合
       print "#{indent}#{obj.class}:#{obj.object_id}\n"
       print "#{indent}[\n"
-      obj.each do |item|
-        dump(item, item.class, level + 1, true) # 各オブジェクトに対して再帰的にdumpを呼び出す
+      obj.each_with_index do |item, index|
+        dump(item, item.class, level + 1, true, dumped_objects, index == 0) # 各オブジェクトに対して再帰的にdumpを呼び出す
       end
       print "#{indent}]\n"
       print "\n" if level == 0 # メソッドの出力の最後に改行を追加
     elsif obj.respond_to?(:attributes)
-      dump(obj.attributes, obj.class, level, false, dumped_objects)
+      dump(obj.attributes, obj.class, level, false, dumped_objects, false)
     else
       print indent + obj.inspect.chomp
       print "\n"  if level == 0 # メソッドの出力の最後に改行を追加
