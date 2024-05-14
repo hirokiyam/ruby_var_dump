5e1ba691161cb6e5c90bd7412dba6e27a7c220ac
hirokiyam <distant_record@hotmail.co.jp>
2024-05-15 02:04:02 +0900

    Fixed output method of ActiveRecord association. Delete argument.

--- ruby_var_dump/lib/ruby_var_dump.rb
+++ ruby_var_dump/lib/ruby_var_dump.rb
@@ -5,7 +5,7 @@ require_relative "ruby_var_dump/version"
 module RubyVarDump
   class Error < StandardError; end
 
-  def dump(obj, klass=obj.class, level = 0, is_value = false, dumped_objects = [], is_first=true)
+  def dump(obj, level = 0, is_value = false, dumped_objects = [], is_first=true)
     indent = ' ' * level * 2
 
     if (defined?(ActiveRecord::Base) && obj.is_a?(ActiveRecord::Base)) || (defined?(ActiveRecord::Relation) && obj.is_a?(ActiveRecord::Relation))
@@ -25,7 +25,7 @@ module RubyVarDump
       else
         print "\n"
         obj.each_with_index do |item, index|
-          dump(item, item.class, level + 1, false, dumped_objects, false)
+          dump(item, level + 1, false, dumped_objects, false)
           print "," unless index == obj.size - 1
           print "\n"
         end
@@ -43,7 +43,7 @@ module RubyVarDump
           # キーにインデントを適用し、値にはインデントを適用しない
           print "#{indent}  #{key.inspect.chomp} => "
           if value.is_a?(Hash) || value.is_a?(Array)
-            dump(value, value.class, level + 1, true, dumped_objects, false)  # ハッシュのバリューの場合には is_value を true に設定
+            dump(value, level + 1, true, dumped_objects, false)  # ハッシュのバリューの場合には is_value を true に設定
           else
             print value.inspect  # プリミティブな値の場合は現在のレベルで出力
           end
@@ -66,10 +66,9 @@ module RubyVarDump
         next if association.macro.nil? # アソシエーションが存在しない場合はスキップ
 
         associated_value = obj.send(association.name)
-        # print "#{indent}  << #{association.name} >> (association):"
         if associated_value.respond_to?(:each) # アソシエーションがコレクションの場合
           next if associated_value.empty?
-          # print("\n#{indent}  [\n")
+
           associated_value.each do |item|
             next if dumped_objects.any? { |dumped_obj| dumped_obj.object_id == item.object_id && dumped_obj.class == item.class }
 
@@ -82,22 +81,19 @@ module RubyVarDump
     
             print("#{indent}  }\n") #ここまでアソシエーションの描画
           end
-          # print("#{indent}  ]\n")
         end
       end
       print "#{indent}}\n"
-      # print "\n" if level == 0
     elsif defined?(ActiveRecord::Relation) && obj.is_a?(ActiveRecord::Relation)
       print "#{indent}#{obj.class}:#{obj.object_id}\n"
       print "#{indent}[\n"
       obj.each_with_index do |item, index|
-        dump(item, item.class, level + 1, false, dumped_objects, index == 0)
-        # print "," if index < obj.size - 1
+        dump(item, level + 1, false, dumped_objects, index == 0)
       end
       print "#{indent}]"
       print "\n" if level == 0 # メソッドの出力の最後に改行を追加
     elsif obj.respond_to?(:attributes)
-      dump(obj.attributes, obj.class, level, false, dumped_objects, false)
+      dump(obj.attributes, level, false, dumped_objects, false)
     else
       print indent + obj.inspect.chomp
       print "\n"  if level == 0 # メソッドの出力の最後に改行を追加
